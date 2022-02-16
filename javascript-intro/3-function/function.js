7b38ebed437ab1ecaac049887dc541e19bb057aa
rontiff <98579166+rontiff@users.noreply.github.com>
2022-02-16 18:39:36 +0800

    update js function lec

---
+++ javascript-intro/3-function/function.js
@@ -0,0 +1,21 @@
+
+// When calling function, it will say hello.
+
+function sayHi(){
+    console.log("Hello")
+}
+
+// call the function
+sayHi()
+
+
+//////////////////////////////////////////////
+
+// how to set parameter in function
+
+function sayHi(name){
+    console.log("Hello "+ name +".")
+}
+sayHi("Ron")
+sayHi("Sara")
+sayHi("Susey")
---
+++ javascript-intro/3-function/property-tax.js
@@ -0,0 +1,43 @@
+
+// function calculate property Tax
+
+function propertyTax(price){
+    if(price<2000000){
+        return price*0.015
+
+    }else if(price>=2000000 && price<2176470){
+        return (30000+(price-2000000)*0.2)
+
+    }else if(price>=2176470 && price<3000000){
+        return(price*0.03)
+
+    }else if(price>=3000000 && price<3290330){
+        return(90000+(price-3000000)*0.2)
+
+    }else if(price>=3290330 && price<4000000){
+        return(price*0.045)
+
+    }else if(price>=4000000 && price<4428580){
+        return(180000+(price-4000000)*0.2)
+
+    }else if(price>=4428580 && price<6000000){
+        return(price*0.06)
+
+    }else if(price>=6000000 && price<6720000){
+        return(360000+(price-6000000)*0.2)
+
+    }else if(price>=6720000 && price<20000000){
+        return(price*0.075)
+
+    }else if(price>=20000000 && price<21739130){
+        return(1500000+(price-20000000)*0.2)
+
+    }else if (price>=21739130){
+        return(price*0.085)
+    }
+}
+
+payment="you need to pay "+propertyTax(1900000)+ "$ for property tax."
+
+console.log(payment)
+
