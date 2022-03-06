31ebd091d532d2301ce0dc4898431bc2a182b363
rontiff <98579166+rontiff@users.noreply.github.com>
2022-03-07 03:07:23 +0800

    update

--- javascript-intro/1-javascript-basic/15-for-loop.js
+++ javascript-intro/1-javascript-basic/15-for-loop.js
@@ -1,1 +1,14 @@
-console.log("hello")
+function fuck(num){
+    if(num>=5){
+        console.log("屌你老母")
+    }else{
+        console.log("下次再玩啦")
+    }
+}
+
+
+let x=0
+    while(x<=10){
+        fuck(x)
+        x+=1
+    }
