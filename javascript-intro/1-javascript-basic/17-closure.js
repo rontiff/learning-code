cf1678057e3db5894fff187b5842fb211c9b61d3
rontiff <98579166+rontiff@users.noreply.github.com>
2022-03-08 01:47:46 +0800

    update

--- javascript-intro/1-javascript-basic/17-closure.js
+++ javascript-intro/1-javascript-basic/17-closure.js
@@ -1,13 +1,46 @@
-
-
-
+//Closure : 有記憶既 function
+//Closure : 可以分開獨立function 
 
 function abc(){
+    let count=0
     return function(){
-        console.log("hi")
+        count= count+1
+        console.log(count)
     }
 }
 
-const returnedFunction=abc()
+const returnedFunction=abc();
 
-returnedFunction()
+returnedFunction() //=1
+returnedFunction() //=2
+returnedFunction() //=3
+
+
+//////////////////////////////////////////////
+
+function createCounter(){
+    let count=0
+    return{
+        add:function(){
+            count=count+1
+        },
+        result:function(){
+            return count;
+        }
+    }
+}
+
+const counter1= createCounter();
+counter1.add()
+counter1.add()
+console.log(counter1.result())
+
+
+
+const counter2= createCounter();
+counter1.add()
+counter2.add()
+
+console.log(counter1.result())
+console.log(counter2.result())
+
---
+++ javascript-intro/1-javascript-basic/19-use-npm.js
@@ -0,0 +1,37 @@
+
+//What is npm? 高手已經寫好咗嘅code, 可以download 黎用
+
+// command
+// Step1: npm init
+//     -多左個file 叫 "package.json"
+
+// Step2: npm install 
+//     -可以用npm 既工具
+
+// Step3: npm install readline-sync
+//     -當npm install 加工具，package.json 會 update
+//     -node_modules 係儲存install 左要用既 npm 工具
+
+//Step4: import 入去JS file 用
+
+const readline = require('readline-sync') //要用readline-sync就要睇說明書
+const busboy = require('busboy')
+const date = require('date-fns')
+
+//example 1 Question
+const name =readline.question("what is your name?")
+console.log("hi "+name+", nice to meet you.")
+
+//example 2 Color
+const colors=["blue","yellow","black","white"]
+const favouriteColor = readline.keyInSelect(colors, "what is your favorite color?")
+console.log("Good taste! your favorite color is "+ colors[favouriteColor]+" color!")
+
+
+//example 3 BMI
+const weight=readline.question("what is your weight in KG?")
+const height=readline.question("what is your height in meter?")
+
+bmiValue=(parseInt(weight)/(parseFloat(height))**2).toFixed(2)
+
+console.log("Your BMI value is "+bmiValue+".")
