803a659c75bf99b5aa7b24f79fa8300c6c36d755
rontiff <98579166+rontiff@users.noreply.github.com>
2022-05-09 19:42:34 +0800

    first java progarm Hello World

---
+++ java/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main{
+    public static void main(String[]args){
+    //write code here. 
+
+        System.out.println("Hello World");
+    }
+}
+//run the program: javac Main.java
+// run the program: java Main
