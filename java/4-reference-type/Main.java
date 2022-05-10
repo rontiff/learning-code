c2549243ee7d0910ead0f398d4a3a63e2b56ab23
rontiff <98579166+rontiff@users.noreply.github.com>
2022-05-10 15:47:59 +0800

    java code

--- java/4-reference-type/Main.java
+++ java/4-reference-type/Main.java
@@ -7,12 +7,16 @@ import java.util.Date;
 public class Main{
     public static void main(String[]args){
         //dont have allocate memory
+        int age=30;
+
         // byte age=30; // no member
         //reference type ALWAYS need tonew opp allocate memory.
         //use new allocate operator 
         Date now=new Date();
         now.getTime();    //member 
         System.out.println(now);
+        System.out.println(age);
+
 
      } 
  }   
