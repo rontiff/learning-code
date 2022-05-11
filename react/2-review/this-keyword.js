ac64b5b087e728d9e353f8d9489d66ed3778cc96
rontiff <98579166+rontiff@users.noreply.github.com>
2022-05-12 00:30:14 +0800

    react

--- react/2-review/this-keyword.js
+++ react/2-review/this-keyword.js
@@ -1,17 +1,25 @@
 
-//what is "this" ?
+//"this" always return a reference to the current object. 
 
 const person={
     name:'sara',
     walk(){
-        console.log(this)    //what is "this" ? 
+        console.log(this)  
     }
 }
+person.walk(); 
+//run "console.log(this)" , so the object will print out.
 
-//"this" always return a reference to the current object. 
+/////////////////////////////////////////////////////////////////////////
+//assign variable
+const walk1 = person.walk.bind(person)
+walk1()
+//bind method = can set "this" permanently
 
-person.walk(); //this is returning a reference to this person object. it print out object.
+//function in JS are Object. While property and method can use in object.
 
+/////////////////////////////////////////////////////////////////////////
 
-const walk = person.walk
-walk()  //undefine, because walk() is method, not function. 
+const walk2 = person.walk
+walk2() 
+//undefine, because walk() is method, not function. 
