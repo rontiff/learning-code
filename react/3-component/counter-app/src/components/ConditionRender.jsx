caa490a37c856db78cf4642c20ca9a81d0b5d1d9
rontiff <98579166+rontiff@users.noreply.github.com>
2022-05-16 23:49:51 +0800

    react

--- react/3-component/counter-app/src/components/ConditionRender.jsx
+++ react/3-component/counter-app/src/components/ConditionRender.jsx
@@ -5,22 +5,24 @@ class ConditionalRender extends Component {
        
     // if no tag element, return "<p>there is no tag</p>"
     state={
-        tag:['tag1','tag2','tag3','tag4']
+        tags:['tag1','tag2','tag3','tag4']
     }
 
     render() { 
         return(
             <React.Fragment>
+                {/* if statement is true, the string will show up.  */}
+                {this.state.tags.length===0 && "Please create a new tag."}
                 {this.renderTag()}
-            </React.Fragment>
+            </React.Fragment> 
         );
     }
 
     renderTag(){
-        if (this.state.tag.length===0){
+        if (this.state.tags.length===0){
             return <p>there is no tag</p>;
-        }else if(this.state.tag.length!==0){
-            return <ol>{this.state.tag.map(function(tag){
+        }else if(this.state.tags.length!==0){
+            return <ol>{this.state.tags.map(function(tag){
                         return <li key={tag}>{tag}</li>
                     })}
                    </ol>
