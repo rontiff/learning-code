e374034080792cea5a368a6e3cf357771f0f5a17
rontiff <98579166+rontiff@users.noreply.github.com>
2022-05-21 15:57:38 +0800

    java

--- java/7-String-special-character/Main.java
+++ java/7-String-special-character/Main.java
@@ -1,6 +1,8 @@
 public class Main{
     public static void main(String[]args){
 
+        //single quote doesn't work. 
+
         //add double quote in string
         String message = "Hello \"Ron\"";
 
