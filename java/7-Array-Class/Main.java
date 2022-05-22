44d17b50b800ee66205a9393b37638b8c2adab1b
rontiff <98579166+rontiff@users.noreply.github.com>
2022-05-22 12:46:48 +0800

    java

--- java/7-Array-Class/Main.java
+++ java/7-Array-Class/Main.java
@@ -1,28 +1,29 @@
 import java.util.Arrays;
-    //array is Reference types
-    //array have fix length
-
-    //array store list of object. 
+    //Arrays is Class in Reference types
+    //Arrays have fix length
+    //Arrays store list of object. 
 
 public class Main{
     public static void main(String[]args){
 
-        //Create integer array (reference type)
+        //Create Integer Arrays (Arrays is Reference Type)
         int[] numbers = new int[5];
 
-        // define what is in the array
+        //Specifically define what is in the array
         numbers[0]=1;
         numbers[1]=2;
         numbers[2]=3;
-        // numbers[10]=3; 
-        //Can't define numbers[10]=3;  
+        //numbers[10]=3 doesn't exist
 
-        //wrong example. java will print out the address.
+        ///////////////////////////////////////////////
+        ///////////////////////////////////////////////
+
+        // Print out the Arrays
+
+        //wrong example
         System.out.println(numbers); 
 
-        //correct example, use "Arrays" class
-        Arrays.toString(numbers);     
-
+        //Correct example (use Arrays Class Method)
         System.out.println(Arrays.toString(numbers));
 
         ///////////////////////////////////////////////
