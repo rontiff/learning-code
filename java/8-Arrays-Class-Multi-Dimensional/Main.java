85fab23af4a2833780323f809ef8b948bb809005
rontiff <98579166+rontiff@users.noreply.github.com>
2022-05-22 14:56:24 +0800

    java

--- java/8-Arrays-Class-Multi-Dimensional/Main.java
+++ java/8-Arrays-Class-Multi-Dimensional/Main.java
@@ -1,43 +1,29 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
-    // for Multi-Dimensional Array, use Arrays.deepToString()  Method
-    // Java can create Multi dimensional Arrays
-    // 2-dimensional array --matrix
-    // 3-dimensional array --cube 
+// Java can create Multi dimensional Arrays
+// for Multi-Dimensional Array, use Arrays.deepToString()  Method
+// 2-dimensional array --matrix
+// 3-dimensional array --cube 
 
 public class Main{
     public static void main(String[]args){
 
-        // vertical x horizontal
         // 2-dimensional 2x3 Matrix  
-        // directly assign value:
-        int [][] matrix = {{1,2,3},{4,5,6}};  //use curly bracket
-        System.out.println(Arrays.deepToString(matrix));
+            int [][]matrix1 = {{1,2,3},{1,2,3}}; //use curly bracket
+            System.out.println(Arrays.deepToString(matrix1));    //[[1, 2, 3], [1, 2, 3]]
+            //use Arrays.deepToString() Method
 
-        //print [[1, 2, 3], [4, 5, 6]]
 
-        /////////////////////////////////////////////////////////////////
-
-        //2-dimensional 2x3 Matrix  
-        int[][] numbers= new int[2][3];
-
-        //assign value
-        numbers[0][0]=1;
-
-        //print 2-dimensional Matrix
-        System.out.println(Arrays.deepToString(numbers));
-
-        ///////////////////////////////////////////////////////////////////////////////
-
+        // 2-dimensional 2x3 Matrix (vertical x horizontal)
+            int [][] matrix2 = new int[2][3];
+            matrix2 [0][0] =1;   //directly assign value:
+            System.out.println(Arrays.deepToString(matrix2));    //[[1, 0, 0], [0, 0, 0]]
+         
+            
         //3-dimensional 2x3x4 cube  
-        int[][][] cube= new int[3][3][4];
+            int [][][] cube = new int[2][3][4];
+            cube [0][0][0] =1;  //directly assign value:
+            System.out.println(Arrays.deepToString(cube));       //[[[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]]
 
-        //assign value
-        cube[0][0][0]=1;
-
-        //print 3-dimensional cube
-        System.out.println(Arrays.deepToString(cube));
      } 
  }   
- 
---
+++ java/9-Constants/Main.java
@@ -0,0 +1,15 @@
+
+//Define Constant using fianl 
+
+public class Main{
+    public static void main(String[]args){
+
+        final int freeze = 273;
+        System.out.println(freeze);
+
+        // 2dp number default Double, need to add suffix "f" at the end
+        final float pi = 3.14159f;
+        System.out.println(pi);
+     } 
+ }   
+ 
