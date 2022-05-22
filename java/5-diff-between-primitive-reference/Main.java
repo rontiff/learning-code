fb4017695051729e70fa65bae14d76a4af4cdcaa
rontiff <98579166+rontiff@users.noreply.github.com>
2022-05-22 10:36:26 +0800

    java

--- java/5-diff-between-primitive-reference/Main.java
+++ java/5-diff-between-primitive-reference/Main.java
@@ -8,26 +8,31 @@ public class Main{
         // Primitive types, store simple number.(integer, number dp)
 
         // Reference Type use Class define variable. can access method. 
-        // Reference Type. dependent because referencing same address.
+        // Reference type store complex object. 
+
+        ////////////////////////////////////////////
+        ////////////////////////////////////////////
 
         byte a=1; 
         byte b=a;  
         a=2;
 
-        System.out.println(b);
-        System.out.println(a);
+        // a & b are independent
+        System.out.println(a); //2
+        System.out.println(b); //1
 
         ////////////////////////////////////////////
         ////////////////////////////////////////////
-
 
         // point1 and point2 are reference same address
-        Point point1= new Point(x:1, y:1);
+        
+        Point point1= new Point(1,1);
         Point point2=point1;
-
         point1.x = 2;
+
+        // point1 & point2 are dependent because referencing same address. 
         System.out.println(point2); // Point[2,1]
-      
+        System.out.println(point1); // Point[2,1]
      } 
  }   
  
