a9b18da7985064a4193959c5252b00236e94cd95
rontiff <98579166+rontiff@users.noreply.github.com>
2022-05-28 14:01:26 +0800

    node

---
+++ node/npm-demo/index.js
@@ -0,0 +1,13 @@
+
+// after npm i underscore
+// import the module
+var _ =require('underscore')  //https://underscorejs.org/
+
+
+var result = _.contains([1,2,3],2);
+var result2 = _.contains([1,2,3],4);
+
+
+console.log(result)
+console.log(result2)
+
