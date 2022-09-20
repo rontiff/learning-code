54447108cd0d12e8184ccfa252f3212ea0260b3e
rontiff <98579166+rontiff@users.noreply.github.com>
2022-09-21 00:17:55 +0800

    js update

--- javascript/javascript-mosh-course/index.js
+++ javascript/javascript-mosh-course/index.js
@@ -1,2 +1,15 @@
-console.log("hello world")
-console.log(1+3)
+
+
+// if 0600-1200; Morning!
+// if 1200-1800; Afternoon!
+// if 1800-0000; night!
+
+let hour=10;
+
+if (hour>=6 && hour<=12){
+    console.log('Morning!');
+}else if(hour>=12 && hour<=18){
+    console.log('Afternoon!');
+}else{
+    console.log('Night!')
+}
