e86b3b32f665fe6b3f60722a4770af5e7bcc447e
rontiff <98579166+rontiff@users.noreply.github.com>
2023-01-07 13:24:15 +0800

    java update

--- java/1-Introduction-Java-Structure/Main.java
+++ java/1-Introduction-Java-Structure/Main.java
@@ -3,10 +3,7 @@
 
 public class Main{
     public static void main(String[]args){
-         System.out.println("Hello World");  //short cut :sout
+         System.out.println("Hello World");   
+        //short cut :sout
      } 
- }   
-
-
-
- 
+ } 
