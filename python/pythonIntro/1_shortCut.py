7ed429a80f2e138f9f4924b8cd0ffea524c62b7b
rontiff <98579166+rontiff@users.noreply.github.com>
2023-03-19 03:47:23 +0800

    python

---
+++ python/pythonIntro/1_shortCut.py
@@ -0,0 +1,11 @@
+
+# linting python code = error searching
+    # debug
+    # ctrl + Shift + M 
+
+# quick execute
+    # ctrl + alt + n
+
+# formatting code
+    # python enhancement proposals (PEPs)
+        # --> view --> command palette --> format document
